1e0560bcdc6bb81bd187b955e5743e8437c85510
Shamrouz Awan <161169144+ShamrouzAwan@users.noreply.github.com>
2024-10-14 22:36:25 +0500

    Update print.py

--- day 1/print.py
+++ day 1/print.py
@@ -1,21 +1,28 @@
 # Day 1: Simple Printing in Python
 
 # The 'print()' function is used to display text or other output in the console.
-# Let's start with some basic print examples.
+# In Python, you can print strings (text), numbers, or a combination of both.
 
 # Printing a simple string
 print("Hello, world!")  # This will output: Hello, world!
 
-# Printing a number
-print(22)  # This will output: 22
+# Printing numbers
+print(42)  # This will output: 42
 
-# Printing a string and a number together
-print("I am 22 years old")  # This will output: I am 22 years old
+# Printing multiple values with commas (Python will add spaces between them)
+print("I am", 22, "years old.")  # This will output: I am 22 years old.
 
-# Printing another simple string
-print("Python is fun!")  # This will output: Python is fun!
+# You can use single or double quotes for strings in Python
+print('Learning Python is fun!')  # This will output: Learning Python is fun!
 
-# You can print any message you'd like.
-# Try printing your name and age below:
-print("My name is Shamrouz.")  # This will output: My name is Shamrouz
-print("I am learning Python!")  # This will output: I am learning Python!
+# Printing multiple lines using escape characters like '\n' for a newline
+print("Line 1\nLine 2\nLine 3")  # This will output:
+# Line 1
+# Line 2
+# Line 3
+
+# Print doesn't add any formatting by itself, so you can use '\t' for tabs as well
+print("Item\tPrice\nApple\t$1\nBanana\t$0.5")  # This will output:
+# Item    Price
+# Apple   $1
+# Banana  $0.5
