423d45b7622556fa914cbd2be708ddbc6e7df9de
Shamrouz Awan <161169144+ShamrouzAwan@users.noreply.github.com>
2024-10-14 22:31:30 +0500

    Learned about printing method

---
+++ day 1/print.py
@@ -0,0 +1,21 @@
+# Day 1: Simple Printing in Python
+
+# The 'print()' function is used to display text or other output in the console.
+# Let's start with some basic print examples.
+
+# Printing a simple string
+print("Hello, world!")  # This will output: Hello, world!
+
+# Printing a number
+print(22)  # This will output: 22
+
+# Printing a string and a number together
+print("I am 22 years old")  # This will output: I am 22 years old
+
+# Printing another simple string
+print("Python is fun!")  # This will output: Python is fun!
+
+# You can print any message you'd like.
+# Try printing your name and age below:
+print("My name is Shamrouz.")  # This will output: My name is Shamrouz
+print("I am learning Python!")  # This will output: I am learning Python!
